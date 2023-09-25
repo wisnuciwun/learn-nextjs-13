8a0a27df211691b0bc5a328070a7ca0f9b14a30e
Wisnu Wardhana <wisnu@tokodistributor.com>
2023-09-25 21:33:36 +0700

    learn part 2 - next routing and replace <a></a> tag use Link (client side nav)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className='display-flex w-100 h-100 justify-center align-items-center bg-purple-400'>
-      Hello world !
+      <div>
+        <h1>Halo guys</h1>
+        <Link href="/user">Pengguna</Link>
+      </div>
     </main>
   )
 }
