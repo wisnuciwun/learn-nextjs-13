81922e95f684e514e17f713ef0c7ea9451a6e5fd
Wisnu Wardhana <wisnu@tokodistributor.com>
2023-09-25 21:26:34 +0700

    learn part 1 - initial commit & project structure

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='display-flex w-100 h-100 justify-center align-items-center bg-purple-400'>
+      Hello world !
+    </main>
+  )
+}
