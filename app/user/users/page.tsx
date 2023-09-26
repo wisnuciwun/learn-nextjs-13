aabfec5e1b6a236ffdf884469d572560ca960cf9
Wisnu Wardhana <wisnu@tokodistributor.com>
2023-09-26 09:59:28 +0700

    learn part 5 - difference rendering SSR with dynamic and static type

--- app/user/users/page.tsx
+++ app/user/users/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 interface Register {
   id: number
@@ -8,14 +9,15 @@ interface Register {
 const Register = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users',
     // caching only exist in fetch, axios not
-    // { cache: 'no-store' } // always get cache data
-    { next: { revalidate: 10 } } // get data every 10s
+    // { cache: 'no-store' } // always get cache data -- ALSO MAKE YOUR BUILD APP RENDERING SERVER SIDE (SSR) AS DYNAMIC (at request time)
+    { next: { revalidate: 10 } } // get data every 10s -- ALSO MAKE YOUR BUILD APP RENDERING SERVER SIDE (SSR) AS STATIC (at build time)
   )
   const userData: Register[] = await res.json() // store in data cache
 
   return (
     <>
       <h1>User</h1>
+      {new Date().toLocaleTimeString()}
       <ul>
         {userData.map(user => <li key={user.id}>{user.name}</li>)}
       </ul>
